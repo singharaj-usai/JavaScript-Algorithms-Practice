6157053243aadf1c96df381f1f2db8c6cf6a83ad
singharaj <djjd22938@gmail.com>
2022-11-28 20:35:03 -0500

    comments

--- 03-log-nums/Unsolved/log-nums.js
+++ 03-log-nums/Unsolved/log-nums.js
@@ -1,6 +1,8 @@
 // Write code to print all numbers from 1 to `num`
 // Assume `num` will be a positive number
 
+// Below is my method that counts down
+
 var logNums = function(num) {
     let start = 1
     while (start <= num) {
@@ -10,3 +12,11 @@ var logNums = function(num) {
 };
 
 logNums(10)
+
+// Below is the solutions that counts up
+
+//var logNums = function(num) {
+//    for (var i = 1; i <= num; i++) {
+//      console.log(i);
+//    }
+//  };
