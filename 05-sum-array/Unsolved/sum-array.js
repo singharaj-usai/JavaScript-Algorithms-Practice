659006599e56828a25fa63a3cba7302f0005c414
singharaj <djjd22938@gmail.com>
2022-11-28 20:57:31 -0500

    finished with sum array

--- 05-sum-array/Unsolved/sum-array.js
+++ 05-sum-array/Unsolved/sum-array.js
@@ -1,3 +1,9 @@
 // Write code to add all the numbers in `arr` and return the total
 
-var sumArray = function(arr) {};
+var sumArray = function (array) {
+  let sum = 0;
+  array.forEach((number) => {
+    return (sum += number);
+  });
+  return sum;
+};
