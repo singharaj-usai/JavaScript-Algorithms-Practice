bbf27602dc421a815223ebea9b0eec65593480cc
singharaj <djjd22938@gmail.com>
2022-11-28 20:30:54 -0500

    done with number logs

--- 03-log-nums/Unsolved/log-nums.js
+++ 03-log-nums/Unsolved/log-nums.js
@@ -1,4 +1,12 @@
 // Write code to print all numbers from 1 to `num`
 // Assume `num` will be a positive number
 
-var logNums = function(num) {};
+var logNums = function(num) {
+    let start = 1
+    while (start <= num) {
+        console.log(start)
+        start++
+    }
+};
+
+logNums(10)
