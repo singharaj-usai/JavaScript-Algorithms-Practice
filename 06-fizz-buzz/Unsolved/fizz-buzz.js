c090b759523fc87b17fae565a6b8e4706581c235
singharaj <djjd22938@gmail.com>
2022-11-28 21:05:21 -0500

    fizz buzz done

--- 06-fizz-buzz/Unsolved/fizz-buzz.js
+++ 06-fizz-buzz/Unsolved/fizz-buzz.js
@@ -4,4 +4,23 @@
 // If a number is evenly divisible by both 3 AND 5, print "Fizz Buzz"
 // If a number is not divisible by 3 or 5, print the number
 
-var fizzBuzz = function(arr) {};
+var fizzBuzz = function (array) {
+  array.forEach((number) => {
+    if (number % 3 === 0 && number % 5 === 0) {
+      console.log("Fizz Buzz");
+      return "Fizz Buzz";
+    } else if (number % 3 === 0) {
+      console.log("Fizz");
+      return "Fizz";
+    } else if (number % 5 === 0) {
+      console.log("Buzz");
+      return "Buzz";
+    } else {
+      console.log(number);
+      return number;
+    }
+  });
+};
+
+const result = fizzBuzz([1, 2, 3, 4, 5, 6, 7, 8, 9, 10, 11, 12, 13, 14, 15]);
+console.log(result);
