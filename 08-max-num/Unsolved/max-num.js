b40fd27e0658d7b616a7d269036a9cf9162e7085
singharaj <djjd22938@gmail.com>
2022-11-28 21:11:13 -0500

    finished max num

--- 08-max-num/Unsolved/max-num.js
+++ 08-max-num/Unsolved/max-num.js
@@ -1,3 +1,14 @@
 // Write code to return the largest number in the given array
 
-var maxNum = function(arr) {};
+var maxNum = function (array) {
+  let max = array[0];
+  for (let i = 0; i < array.length; i++) {
+    const currentNumber = array[i];
+    if (max < currentNumber) {
+      max = currentNumber;
+    }
+  }
+  return max;
+};
+
+console.log(maxNum([12, 63, 200, 5]));
