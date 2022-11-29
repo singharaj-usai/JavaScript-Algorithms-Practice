c432d40b8cb0392924478f7bfd2afa1a6c86b604
singharaj <djjd22938@gmail.com>
2022-11-28 21:16:33 -0500

    done with vowel count and practice for today

--- 09-vowel-count/Unsolved/vowel-count.js
+++ 09-vowel-count/Unsolved/vowel-count.js
@@ -1,3 +1,25 @@
 // Write code to return the the number of vowels in `str`
 
-var vowelCount = function(str) {};
+var vowelCount = function (str) {
+  const splitstring = str.toLowerCase().split("");
+  console.log(splitstring);
+  let count = 0;
+
+  splitstring.forEach((letter) => {
+    if (
+      letter === "a" ||
+      letter === "e" ||
+      letter === "i" ||
+      letter === "o" ||
+      letter === "u"
+    ) {
+      console.log("Vowel found, increasting count");
+      count++;
+    }
+  });
+  return count;
+};
+
+// console.log("a" === "j" || "i");
+
+//The above log will NOT compare a to both j and i, but rather log "i" because "a" does not equal "j" (falsey)
