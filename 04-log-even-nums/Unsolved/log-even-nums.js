7fcbf4d4dca64962464c660210172a10ffb68c6c
singharaj <djjd22938@gmail.com>
2022-11-28 20:48:07 -0500

    evens log done

--- 04-log-even-nums/Unsolved/log-even-nums.js
+++ 04-log-even-nums/Unsolved/log-even-nums.js
@@ -1,4 +1,14 @@
 // Write code to print all even numbers from 0 to `num`
 // Assume `num` will be a positive number
 
-var logEvenNums = function(num) {};
+var logEvenNums = function (num) {
+  let start = 0;
+  while (start < num) {
+    if (start % 2 === 0) {
+      console.log(start);
+      start++;
+    } else {
+      start++;
+    }
+  }
+};
