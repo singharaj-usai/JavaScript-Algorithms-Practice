4f28417c02441876855668836b254af339b22774
singharaj <djjd22938@gmail.com>
2022-11-29 21:09:32 -0500

    done!!

--- 17-is-unique/Unsolved/is-unique.js
+++ 17-is-unique/Unsolved/is-unique.js
@@ -1,4 +1,21 @@
 // Write code to create a function that accepts an array numbers
 // Return `true` is no number appears in the array more than once, else return `false`
 
-var isUnique = function(arr) {};
+var isUnique = function (array) {
+  arrayObj = {};
+  for (let i = 0; i < array.length; i++) {
+    const number = array[i];
+    if (!arrayObj[number]) {
+      arrayObj[number] = 1;
+    } else {
+      arrayObj[number]++;
+    }
+    if (arrayObj[number] > 1) {
+      return false;
+    }
+  }
+  // Returns true if there are no numbers appearing in the array more than once
+  return true;
+};
+
+console.log(isUnique([1, 2, 3, 4, 5]));
