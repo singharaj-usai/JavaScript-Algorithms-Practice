8adbbc00faf6b0236ad1ca2e2b0fffa727e9f077
singharaj <djjd22938@gmail.com>
2022-11-29 21:24:39 -0500

    done for now

--- 19-most-songs-in-playlist/Unsolved/most-songs-in-playlist.js
+++ 19-most-songs-in-playlist/Unsolved/most-songs-in-playlist.js
@@ -2,6 +2,21 @@
 // Each number represents the duration of a song in minutes
 // Return the maximum number of songs from the list that can fit in a 60 minute playlist
 
-var mostSongsInPlaylist = function(arr) {
-  // add your code here...
+var mostSongsInPlaylist = function (array) {
+  var sortedArray = array.sort();
+  let songLengthTotal = 0;
+  let songCount = 0;
+
+  for (let i = 0; i < sortedArray.length; i++) {
+    const songLength = sortedArray[i];
+
+    if (songLengthTotal + songLength < 60) {
+      songLengthTotal = songLengthTotal + songLength;
+      songCount++;
+      console.log(`Total length: ${songLengthTotal} minutes`);
+    } else {
+      return songCount;
+    }
+  }
+  return songCount;
 };
