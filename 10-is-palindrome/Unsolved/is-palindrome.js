8b97395ffda01685da4a9a84ae21214d69993cda
singharaj <djjd22938@gmail.com>
2022-11-29 20:04:42 -0500

    palindrome done

--- 10-is-palindrome/Unsolved/is-palindrome.js
+++ 10-is-palindrome/Unsolved/is-palindrome.js
@@ -1,3 +1,15 @@
 // Write code that returns true if `str` is a palindrome, and false if `str` is not a palindrome
 
-var isPalindrome = function(str) {};
+var isPalindrome = function (str) {
+  // lcstring is LowerCase String
+  const lcstring = str.toLowerCase();
+  const splitstring = lcstring.split("");
+  const newstring = splitstring.reverse().join("");
+  if (lcstring === newstring) {
+    return true;
+  } else {
+    return false;
+  }
+};
+
+console.log(isPalindrome("racecar"));
