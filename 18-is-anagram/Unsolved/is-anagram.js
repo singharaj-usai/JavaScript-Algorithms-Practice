a51d1d2cd6cfd39520d1246251797184ba084035
singharaj <djjd22938@gmail.com>
2022-11-29 21:18:01 -0500

    anagram test passes

--- 18-is-anagram/Unsolved/is-anagram.js
+++ 18-is-anagram/Unsolved/is-anagram.js
@@ -1,4 +1,36 @@
 // Write code to create a function that accepts two strings
 // Return true if the strings are anagrams of each other, otherwise return false
 
-var isAnagram = function(strA, strB) {};
+var isAnagram = function(strA, strB) {
+    objA = {}
+    objB = {}
+    
+    for (let i = 0; i < strA.length; i++){
+        const character = strA[i];
+        if (!objA[character]) {
+            objA[character] = 1;
+        } else {
+            objA[character]++;
+        }
+    }
+
+    for (let i = 0; i < strB.length; i++){
+        const character = strB[i];
+        if (!objB[character]) {
+            objB[character] = 1;
+        } else {
+            objB[character]++
+        }
+    }
+
+    const objALength = Object.keys(objA).length;
+    const objBLength = Object.keys(objB).length;
+
+    if (objALength === objBLength) {
+        return Object.keys(objA).every( key =>
+            objB.hasOwnProperty(key) && objB[key] === objA[key]);
+    }
+    return false;
+};
+
+console.log(isAnagram("Test yooo", "Test yo"));
