39c4be1f5f32515baa936fab5dd56cc79d0088e9
singharaj <djjd22938@gmail.com>
2022-11-29 20:56:28 -0500

    camel case assignment dooooone

--- 15-camel-case/Unsolved/camel-case.js
+++ 15-camel-case/Unsolved/camel-case.js
@@ -1,3 +1,19 @@
 // Write code to create a function that accepts a string and returns the string in camelCase
 
-var camelCase = function(str) {};
+var camelCase = function(str) {
+    newString = str.split(" ")
+    newWords = []
+    firstWord = newString[0].toLowerCase()
+    neWwords.push(firstWord)
+    for (let i = 1; i < newString.length; i++){
+        const word = newString[i]
+        letters = word.split("")
+        letters[0] = letters[0].toUpperCase();
+        const newWord = letters.join("")
+        newWords.push(newWord)
+    }
+    return neWwords.join("")
+};
+
+// The first word should not be capitalized
+console.log(camelCase("I love jillian"))
