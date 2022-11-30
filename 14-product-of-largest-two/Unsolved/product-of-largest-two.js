b6b62d324d43fc131c244d0ddf1b416c987007ea
singharaj <djjd22938@gmail.com>
2022-11-29 20:46:20 -0500

    test passes for productOfLargestTwo

--- 14-product-of-largest-two/Unsolved/product-of-largest-two.js
+++ 14-product-of-largest-two/Unsolved/product-of-largest-two.js
@@ -1,3 +1,19 @@
 // Write code to create a function that accepts an array of numbers, finds the largest two numbers, and returns the product of the two
 
-var productOfLargestTwo = function(arr) {};
+var productOfLargestTwo = function (array) {
+  let max = array[0];
+  let secondmax = array[0];
+
+  for (let i = 0; i < array.length; i++) {
+    const number = array[i];
+    if (number > max) {
+      secondmax = max;
+      max = number;
+    } else if (number > secondmax) {
+      secondmax = number;
+    }
+  }
+  return max * secondmax;
+};
+
+console.log(productOfLargestTwo([1, 2, 300, 4, 5]));
