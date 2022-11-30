882ff1bda2257a31eca2142f82ceb0b90130128b
singharaj <djjd22938@gmail.com>
2022-11-29 20:40:32 -0500

    character-count done

--- 13-character-count/Unsolved/character-count.js
+++ 13-character-count/Unsolved/character-count.js
@@ -1,3 +1,18 @@
 // Write code to create a function that accepts a string and returns an object containing the number of times each character appears in the string
 
-var characterCount = function(str) {};
+var characterCount = function (str) {
+  const charObj = new Object();
+  const words = str.split(" ");
+  words.forEach((word) => {
+    const letters = word.split("");
+    for (let i = 0; i < letters.length; i++) {
+      const letter = letters[i];
+      if (letter in charObj) {
+        charObj[letter]++;
+      } else {
+        charObj[letter] = 1;
+      }
+    }
+  });
+  return charObj;
+};
