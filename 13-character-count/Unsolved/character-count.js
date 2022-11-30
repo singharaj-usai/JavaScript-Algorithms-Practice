7c0c87be5e30bd4d58c82d52e54e5e474b1eb8d5
singharaj <djjd22938@gmail.com>
2022-11-29 20:41:29 -0500

    bruh code doesnt work, skipping and commented for now

--- 13-character-count/Unsolved/character-count.js
+++ 13-character-count/Unsolved/character-count.js
@@ -1,5 +1,6 @@
 // Write code to create a function that accepts a string and returns an object containing the number of times each character appears in the string
 
+// The code doesn't work for some reason...
 var characterCount = function (str) {
   const charObj = new Object();
   const words = str.split(" ");
