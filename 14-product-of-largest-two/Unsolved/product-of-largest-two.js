406fa0cd3b851480a93fbbed2597c47638e99e9f
singharaj <djjd22938@gmail.com>
2022-11-29 20:48:14 -0500

    for loop comment and let comment

--- 14-product-of-largest-two/Unsolved/product-of-largest-two.js
+++ 14-product-of-largest-two/Unsolved/product-of-largest-two.js
@@ -1,9 +1,12 @@
 // Write code to create a function that accepts an array of numbers, finds the largest two numbers, and returns the product of the two
 
 var productOfLargestTwo = function (array) {
+  // Setting the variables for largest and second largest numbers
   let max = array[0];
   let secondmax = array[0];
 
+  // For loop to check if the number is larger than max
+  // If not larger, check if it's larger than secondmax
   for (let i = 0; i < array.length; i++) {
     const number = array[i];
     if (number > max) {
