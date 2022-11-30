ff555b2c8b84e2b8722267d7dab40890efd16763
singharaj <djjd22938@gmail.com>
2022-11-29 20:31:14 -0500

    title case done

--- 12-title-case/Unsolved/title-case.js
+++ 12-title-case/Unsolved/title-case.js
@@ -1,3 +1,18 @@
 // Write code to create a function takes a string and returns a new string with the first letters of each word capitalized
 
-var titleCase = function(str) {};
+var titleCase = function (str) {
+  // Quotes need space for title cases
+  const words = str.split(" ");
+  let newstring = [];
+  words.forEach((word) => {
+    letters = word.split("");
+    letters[0] = letters[0].toUpperCase();
+    console.log(letters[0]);
+    const newword = letters.join("");
+    newstring.push(newword);
+  });
+  console.log(newstring);
+  return newstring.join(" ");
+};
+
+console.log(titleCase("I am da goat"));
