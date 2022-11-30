1e1a4e72941ad7e0180de79e50cbc4943588b3aa
singharaj <djjd22938@gmail.com>
2022-11-29 20:15:54 -0500

    factorial done

--- 11-factorial/Unsolved/factorial.js
+++ 11-factorial/Unsolved/factorial.js
@@ -1,3 +1,13 @@
 // Write code to create a function that returns the factorial of `num`
 
-var factorial = function(num) {};
+var factorial = function (num) {
+  // If equals to 0, then product will always be 0, so start at 1
+  let factorial = 1;
+  while (num >= 1) {
+    factorial *= num;
+    num--;
+  }
+  console.log(factorial);
+  return factorial;
+};
+console.log(factorial(0));
