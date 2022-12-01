81db519aaa97b8af211ab186069fd16a17be227b
singharaj <djjd22938@gmail.com>
2022-11-30 21:17:08 -0500

    array search finished

--- 21-array-search-2d/Unsolved/array-search-2d.js
+++ 21-array-search-2d/Unsolved/array-search-2d.js
@@ -2,4 +2,18 @@
 // Each 2D element contains either the string "X" or "O" (both capitalized)
 // Return the number of times "X" appears in the 2D array
 
-var arraySearch2D = function(arr) {};
+// Concatenate nested arrays as one large parent array
+var arraySearch2D = function (array) {
+  var newArray = [].concat(...array);
+  console.log(newArray);
+  let count = 0;
+  for (let i = 0; i < newArray.length; i++) {
+    const e = newArray[i];
+    if (e === "X") {
+      count++;
+    }
+  }
+  return count;
+};
+
+console.log(arraySearch2D(array));
