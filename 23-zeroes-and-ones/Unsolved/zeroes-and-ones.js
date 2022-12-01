d6864a78e227e0388e0df3497465a2861779cef1
singharaj <djjd22938@gmail.com>
2022-11-30 21:33:46 -0500

    zeroes and ones

--- 23-zeroes-and-ones/Unsolved/zeroes-and-ones.js
+++ 23-zeroes-and-ones/Unsolved/zeroes-and-ones.js
@@ -2,4 +2,27 @@
 // Return true if there are an equal number of 0s and 1s
 // Else return false
 
-var zeroesAndOnes = function(str) {};
+var zeroesAndOnes = function (str) {
+  strMap = {};
+  for (let i = 0; i < str.length; i++) {
+    const e = str[i];
+    if (strMap[e]) {
+      strMap[e]++;
+    } else {
+      strMap[e] = 1;
+    }
+  }
+  console.log(strMap);
+  // Checks if string contains more than two types of characters.
+  // String should only contain 0's and 1's
+  // length of ensuing array shouldn't be greater than 2.
+  if (Object.keys(strMap).length > 2) {
+    return false;
+  } else if (strMap["0"] === strMap["1"]) {
+    return true;
+  } else {
+    return false;
+  }
+};
+
+console.log(zeroesAndOnes(""));
