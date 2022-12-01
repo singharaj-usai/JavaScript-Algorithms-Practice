9b0a917993ee0a70f3144f80eecee26f3e7c785d
singharaj <djjd22938@gmail.com>
2022-11-30 21:08:19 -0500

    perfect square done

--- 20-is-perfect-square/Unsolved/is-perfect-square.js
+++ 20-is-perfect-square/Unsolved/is-perfect-square.js
@@ -1,4 +1,16 @@
 // Write code to create a function that accepts a number and returns true if the number is a perfect square, otherwise it returns false.
 // You may not use the built-in Math.sqrt method
 
-var isPerfectSquare = function(num) {};
+var isPerfectSquare = function(num) {
+    let squareRoot = Math.sqrt(num)
+    console.log(squareRoot)
+    let roundUp = Math.ceil(Math.sqrt(num))
+    let roundDown = Math.floor(Math.sqrt(num))
+    if (roundUp === roundDown) {
+        return true
+    } else {
+        return false
+    }
+};
+
+isPerfectSquare(14)
