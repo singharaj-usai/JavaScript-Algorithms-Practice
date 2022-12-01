1cb2ee16ed4c71263fbd132597b90d58b9f137c9
singharaj <djjd22938@gmail.com>
2022-11-30 21:26:54 -0500

    done with this

--- 22-multiply-into-20/Unsolved/multiply-into-20.js
+++ 22-multiply-into-20/Unsolved/multiply-into-20.js
@@ -1,3 +1,26 @@
-const multiplyInto20 = function(arr) {
+const multiplyInto20 = function (array) {
   // TODO: Write Your Code Here
+  // The first loop isn't necessary, 
+  // I just wanted some practice using objects to map values.
+  let arrayMap = {};
+  for (let i = 0; i < array.length; i++) {
+    const number = array[i];
+    if (20 % number === 0) {
+      arrayMap[number] = true;
+    }
+  }
+
+  let factors = Object.keys(arrayMap);
+  for (let i = 0; i < factors.length; i++) {
+    const numberOne = factors[i];
+    for (let i = 0; i < factors.length; i++) {
+      const numberTwo = factors[i];
+      if (numberOne * numberTwo === 20) {
+        return true;
+      }
+    }
+  }
+  return false;
 };
+
+console.log(multiplyInto20([1, 2, 3, 4, 5, 7, 9]));
