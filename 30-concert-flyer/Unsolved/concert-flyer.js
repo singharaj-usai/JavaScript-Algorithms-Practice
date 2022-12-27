0f757a18ac8ff8bacda1d1e06a8cab2dd8a24145
singharaj <djjd22938@gmail.com>
2022-12-27 12:52:50 -0500

    test passes, copy and pasted same things previously but with split methods now

--- 30-concert-flyer/Unsolved/concert-flyer.js
+++ 30-concert-flyer/Unsolved/concert-flyer.js
@@ -1,3 +1,26 @@
 // Write a function that takes two strings and returns true if every word found in the second string is present in the first string. You will be checking for both words and their frequency. Assume you'll need to worry about casing, but the strings won't contain any punctuation. Assume neither string will be empty
 
-var concertFlyer = function (magazine, flyer) {};
+var concertFlyer = function (magazine, flyer) {
+    wordMap = {};
+    let magWords = magazine.split(" ");
+    let flyerWords = flyer.split(" ");
+
+    for (let i = 0; i < magWords.length; i++) {
+        const word = magWords[i];
+        if (wordMap[word]) {
+            wordMap[word]++;
+        } else {
+            wordMap[word] = 1;
+        }
+    }
+
+    for (let i = 0; i < flyerWords.length; i++) {
+        const word = flyerWords[i];
+        if (!wordMap[word]) {
+            return false;
+        }
+    }
+
+    console.log(wordMap);
+    return true;
+};
