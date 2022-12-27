aacf41b88c32af22d986ecfa04a7fbc83cc2dae4
singharaj <djjd22938@gmail.com>
2022-12-27 12:37:12 -0500

    done

--- 27-squares-of-a-sorted-array/Unsolved/squares-of-a-sorted-array.js
+++ 27-squares-of-a-sorted-array/Unsolved/squares-of-a-sorted-array.js
@@ -1,3 +1,24 @@
 // Write code to create a function that accepts an array of integers sorted in ascending (increasing) order and returns a new array containing the squares of each number in ascending order
 
-var sortedSquares = function(arr) {};
+var sortedSquares = function(arr) {
+    const result = arr.map((number) => {
+        return number * number;
+    });
+
+    console.log(`Pre sort: ${result}`);
+
+    for (let i = 0; i < result.length; i++) {
+        for (let j = 0; j < result.length; j++) {
+            if (result[i] < result [j]) {
+                console.log(`Comparing ${result[i]} to ${result[j]}`)
+                var temp = result[i];
+                result[i] = result[j];
+                result[j] = temp;
+            }
+        }
+    }
+    console.log(`Post sort: ${result}`);
+    return result;
+};
+
+console.log(sortedSquares([-4,-1,0]));
