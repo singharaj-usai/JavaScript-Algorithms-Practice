a13537fb69137c64588b421c3847e32b5d55c79f
singharaj <djjd22938@gmail.com>
2022-12-27 12:49:31 -0500

    just copied and paste stuff from array-intersection and reword some stuff, it works

--- 29-permutation-substring/Unsolved/permutation-substring.js
+++ 29-permutation-substring/Unsolved/permutation-substring.js
@@ -2,4 +2,25 @@
 // Return true if the second string is a substring of any permutation of the first string
 // Else return false
 
-var permutationSubstring = function(str, sub) {};
+var permutationSubstring = function (str, sub) {
+    strMap = {};
+    subMap = {};
+
+    for (let i = 0; i < str.length; i++) {
+        const character = str[i];
+        if (strMap[character]) {
+            strMap[character]++;
+        } else {
+            strMap[character] = 1;
+        }
+    }
+
+    for (let i = 0; i < sub.length; i++) {
+        const character = sub[i];
+        if (!strMap[character]) {
+            return false;
+        }
+    }
+
+    return true;
+};
