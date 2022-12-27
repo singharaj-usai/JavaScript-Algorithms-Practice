dca3526f4d3f4612a808d48a0db0c89c7335a391
singharaj <djjd22938@gmail.com>
2022-12-27 12:44:47 -0500

    function passes

--- 28-common-element/Unsolved/common-element.js
+++ 28-common-element/Unsolved/common-element.js
@@ -3,4 +3,27 @@
 // Return the common number
 // You may not use the `indexOf` or `includes` method
 
-var commonElement = function(arrA, arrB) {};
+var commonElement = function(arrA, arrB) {
+    let numMap = {};
+    let result;
+
+    for (let i = 0; i < arrA.length; i++) {
+        const number = arrA[i];
+        if (numMap[number]) {
+            numMap[number]++;
+        } else {
+            numMap[number] = 1;
+        }
+    }
+
+    for (let i = 0; i < arrB.length; i++) {
+        const number = arrB[i];
+        if (numMap[number]) {
+            result = number;
+        }
+    }
+
+    return result;
+};
+
+console.log(commonElement(arrA, arrB));
