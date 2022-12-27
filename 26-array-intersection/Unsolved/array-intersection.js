08d0654249022e188a4e1e954dcd8d5fa9dc0fd0
singharaj <djjd22938@gmail.com>
2022-12-27 12:31:07 -0500

    done

--- 26-array-intersection/Unsolved/array-intersection.js
+++ 26-array-intersection/Unsolved/array-intersection.js
@@ -1,4 +1,27 @@
 // Write code to create a function that accepts two arrays of numbers
 // Return a new array containing the intersecting elements of the arrays
 
-var arrayIntersection = function(arr1, arr2) {};
+var arrayIntersection = function (arr1, arr2) {
+    result = [];
+    numMap = {};
+
+    for (let i = 0; i < arr1.length; i++) {
+        const number = arr1[i];
+        if (numMap[number]) {
+            numMap[number]++;
+        } else {
+            numMap[number] = 1;
+        }
+    }
+
+    for (let i = 0; i < arr2.length; i++) {
+        const number = arr2[i];
+        if (numMap[number]) {
+            result.push(number);
+        }
+    }
+
+    return result;
+};
+
+console.log(arrayIntersection([1, 2, 2, 3, 4, 4], [1, 2, 2, 3, 5]));
