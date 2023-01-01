4d2a4dc35a7948517dc02b9b3ecdadb210892be2
singharaj <djjd22938@gmail.com>
2022-12-31 22:18:35 -0500

    test passes

--- 31-reverse-no-built-in/Unsolved/reverse-no-built-in.js
+++ 31-reverse-no-built-in/Unsolved/reverse-no-built-in.js
@@ -1,4 +1,13 @@
 // Write code to return a new string with all of the characters in `str` reversed.
 // You may NOT use the built-in reverse method
 
-const reverse = (str) => {};
+const reverse = (str) => {
+    let reversed = [];
+    for (let i = str.length-1; i >= 0; i--) {
+        const character = str[i];
+        reversed.push(character);
+    }
+
+    const result = reversed.join("");
+    return result;
+};
