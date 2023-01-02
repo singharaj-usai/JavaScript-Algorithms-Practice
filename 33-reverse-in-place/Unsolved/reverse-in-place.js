bfd9c26f4e10b9173711b3dd8235056ea941b17b
singharaj <djjd22938@gmail.com>
2023-01-02 17:07:26 -0500

    the easy one

--- 33-reverse-in-place/Unsolved/reverse-in-place.js
+++ 33-reverse-in-place/Unsolved/reverse-in-place.js
@@ -1,4 +1,19 @@
 // Write code to return a new string with all of the characters in `str` reversed.
 // You may NOT use the built-in reverse method
 
-const reverseInPlace = (arr) => {};
+const reverseInPlace = (arr) => {
+    let start = 0;
+    let end = arr.length-1;
+
+    while (start < end) {
+        var temp = arr[start];
+        arr[start] = arr[end];
+        arr[end] = temp;
+        start++;
+        end--;
+    }
+
+    return arr;
+};
+
+console.log(arr)
