d8d6df430087775a94cd6a68fd08a72d3423a341
singharaj <djjd22938@gmail.com>
2023-01-05 00:40:41 -0500

    min increment done

--- 35-min-increment-for-unique/Unsolved/min-increment-for-unique.js
+++ 35-min-increment-for-unique/Unsolved/min-increment-for-unique.js
@@ -1,3 +1,16 @@
 // Write a function to take an array of integers and return the minimum number of increments required across the array to ensure that every number is unique
 
-const minIncrement = function(nums) {};
+const minIncrement = function(nums) {
+    let counter = 0;
+    //for each check if number is unique
+    for (let i = 0; i < nums.length; i++) {
+        for (let f = 0; f < nums.length; f++) {
+            if (nums[i] === nums[f] && i !== f) {
+                nums[i]++;
+                counter++;
+                f = i = 0;
+            }
+        }
+    }
+    return counter;
+};
