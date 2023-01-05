8a355c0d0f396cddcd2e6608c715b5435f054097
singharaj <djjd22938@gmail.com>
2023-01-05 00:34:02 -0500

    done

--- 34-valid-brackets/Unsolved/valid-brackets.js
+++ 34-valid-brackets/Unsolved/valid-brackets.js
@@ -1,3 +1,35 @@
 // Write a function to take a string and return true if it contains valid sets of matching brackets, else return false
 
-const validBrackets = function(str) {};
+const validBrackets = function(str) {
+    let strStack = [];
+
+    for (let i = 0; i < str.length; i++) {
+        const e = str[i];
+        if (e === "{" || e === "(" || e === "[" ) {
+            strStack.push(e);
+        }
+
+        if (e === ")") {
+            if (strStack.pop() !== "(") {
+                return false;
+            }
+            continue;
+        }
+
+        if (e === "}") {
+            if (strStack.pop() !== "{") {
+                return false;
+            }
+            continue;
+        }
+
+        if (e === "]") {
+            if (strStack.pop() !== "[") {
+                return false;
+            }
+            continue;
+        }
+    }
+
+    return true;
+};
